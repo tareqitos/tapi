c0c8205b863984bc505ad1bc4aa10dd2f5cb16ac
tareqitos <contact@tareqitos.com>
2025-02-25 18:31:21 +0100

    Implement basic authentication

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,26 +1,74 @@
+"use client"
+
+import { userLogin } from "@/app/hooks/auth";
 import "@/styles/auth.scss"
 import Link from "next/link"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Login() {
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState();
+    const [message, setMessage] = useState('');
+    const [success, setSuccess] = useState(false)
+    const router = useRouter()
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        const { response, result } = await userLogin(email, password);
+        console.log(result)
+
+        switch (response.status) {
+            case 200:
+                console.log('WELCOME')
+                setSuccess(true)
+                setMessage(result.message)
+                localStorage.setItem('accessToken', result.token)
+                await new Promise(resolve => setTimeout(resolve, 3000));
+                router.push('/')
+                console.log(result.message)
+                break;
+
+            case 400:
+                setMessage(result.message)
+                console.log(result.message)
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <Link href='/?' className="form-bg"></Link>
             <div className="form-container form-container-login">
-                <form className="user-form">
-                    <h1>Log in</h1>
-                    <div className="fields-container-login">
-                        <div className="field">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" name="email" required />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" name="password" required />
-                            <Link href="/">Forgot password?</Link>
+                {success ?
+                    <div className="register-login-success">
+                        <div>Welcome back to <span className="title">Tapi!</span></div>
+                    </div>
+                    :
+                    <div>
+                        <form className="user-form" method="POST" onSubmit={handleLogin}>
+                            <h1>Log in</h1>
+                            <div className="fields-container-login">
+                                <div className="field">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
+                                    <Link href="/">Forgot password?</Link>
+                                </div>
+                            </div>
+                            <button type="button submit" className="button">Log in</button>
+                        </form>
+                        <div className={`warning-messages ${message ? 'show' : ''}`}>
+                            {message && <p className="warning">{message}</p>}
                         </div>
                     </div>
-                    <button type="button submit" className="button">Log in</button>
-                </form>
+                }
             </div>
         </>
     )
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,32 +1,91 @@
+"use client"
+
+import { userRegister } from "@/app/hooks/auth"
 import "@/styles/auth.scss"
 import Link from "next/link"
+import { useState } from "react"
 
 export default function Register() {
+    const [email, setEmail] = useState()
+    const [fullname, setFullname] = useState();
+    const [password, setPassword] = useState();
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState(['']);
+    const [success, setSuccess] = useState(false)
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const { response, result } = await userRegister(fullname, email, password);
+        console.log(response)
+
+        switch (response.status) {
+            case 200:
+                console.log('WELCOME')
+                setSuccess(true)
+                setMessage(result.message)
+                console.log(result.message)
+                break;
+
+            case 401:
+                setMessage(result.message)
+                console.log(result.message)
+                break;
+
+            case 403:
+                setMessages(result.message)
+                console.log(result.message)
+                break;
+
+            case 500:
+                setMessage(result.message)
+                console.log(result.message)
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <div className="form-container">
-                <form className="user-form">
-                    <h1>Sign in</h1>
-                    <div className="fields-container">
-                        <div className="field">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" id="username" name="username" required />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" id="email" name="email" required />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" id="password" name="password" required />
-                        </div>
-                        <div className="field">
-                            <label htmlFor="verifyPassword">Verify Password</label>
-                            <input type="password" id="verifyPassword" name="verifyPassword" required />
+                {success ?
+                    <div className="register-login-success">
+                        <div>Welcome to <span className="title">Tapi!</span></div>
+                        <div>
+                            <Link href="/?login=true">
+                                <button className="button login">Log In</button>
+                            </Link>
                         </div>
                     </div>
-                    <button type="button submit" className="button">Register</button>
-                </form>
+                    :
+                    <div>
+                        <form className="user-form" method="POST" onSubmit={handleRegister}>
+                            <h1>Sign in</h1>
+                            <div className="fields-container">
+                                <div className="field">
+                                    <label htmlFor="fullname">Full Name</label>
+                                    <input type="text" id="fullname" name="fullname" onChange={(e) => setFullname(e.target.value)} required />
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
+                                </div>
+                            </div>
+                            <button type="button submit" className="button">Register</button>
+                        </form>
+                        <div className={`warning-messages ${message || messages.length > 0 ? 'show' : ''}`}>
+                            {message && <p className="warning">{message}</p>}
+                            {messages && messages.length > 0 && messages.map((elements, i) => (
+                                <p className="warning" key={i}>{elements.message}</p>
+                            ))}
+                        </div>
+                    </div>
+                }
             </div>
         </>
     )
