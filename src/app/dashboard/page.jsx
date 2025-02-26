01f1f70d3f4b43190b3fbfafd789bebfdf9b771c
Pawulina <paulinawalcz94@gmail.com>
2025-02-26 16:10:16 +0100

    Mobile responsive layout.

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,32 +1,39 @@
-'use client'
+"use client";
 
-import "@/styles/dashboard.scss"
+import "@/styles/dashboard.scss";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import WordsMastered from "@/components/WordsMastered";
+import SessionsChart from "@/components/SessionsChart";
 
+export default function Dashboard() {
+	const [userName, setUserName] = useState("");
+	const router = useRouter();
 
-export default function Dashboard(){
-  const [userName, setUserName] = useState("");
-  const router = useRouter()
+	// useEffect(()=>{
+	//     const token=localStorage.getItem("token");
 
-useEffect(()=>{
-    const token=localStorage.getItem("token");
+	// // if there is no token in localstorage, then push user to login page - check this endpoint!!
+	//     if(!token){
+	//         router.push('/login');
+	//         return
+	//     }
 
+	//     const storedUserName=localStorage.getItem("userName")
+	//     setUserName(storedUserName)
+	// }, [router])
 
-// if there is no token in localstorage, then push user to login page - check this endpoint!!
-    if(!token){
-        router.push('/login');
-        return
-    }
-
-    const storedUserName=localStorage.getItem("userName")
-    setUserName(storedUserName)
-}, [router])
-
-
-    return(
-        <>
-       <h2>Welcome back, {userName} </h2>
-        </>
-    )
+	return (
+		<div className="dashboard-container">
+			<h2 className="greeting">Welcome back, {userName} </h2>
+			<div className="dashboardColumn1-container">
+				<WordsMastered />
+				<SessionsChart />
+			</div>
+			<div className="dashboardColumn2-container">
+				<button>Learn English</button>
+				<button>Learn Polish</button>
+			</div>
+		</div>
+	);
 }
--- src/components/WordsMastered.jsx
+++ src/components/WordsMastered.jsx
@@ -1,13 +1,12 @@
-"use client"
+"use client";
 
-export default function WordsMastered(){
-const masteredWords
-
-
-    return(
-        <>
-        <p>You mastered {wordsNumber} words. <br />
-        Keep going!</p>
-        </>
-    )
+export default function WordsMastered() {
+	return (
+		<div className="masteredWords">
+			<p>
+				You mastered <strong>wordsNumber</strong> words. <br />
+				Keep going!
+			</p>
+		</div>
+	);
 }
