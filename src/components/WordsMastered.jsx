88f6b198814ec926a7da67fe704810709baf67e3
Pawulina <paulinawalcz94@gmail.com>
2025-03-09 18:50:56 +0100

    some changes in words mastered component

--- src/components/WordsMastered.jsx
+++ src/components/WordsMastered.jsx
@@ -1,6 +1,19 @@
 "use client";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+import { useState, useEffect } from "react";
 
 export default function WordsMastered() {
+    const [wordsMastered, setWordsMastered]=useState(0)
+
+    // fill in correct endpoint!!
+    // UserId is needed to fetch correct data
+    // ===========================
+    useEffect(()=>{
+        fetch('${API_URL}/user/wordsMastered')
+        .then
+    })
+
+
 	return (
 		<div className="masteredWords">
 			<p>
